21dbbb51226d8500c64cb71f2b8a1039bc1be748
Frederic Hanslik <frederic.hanslik@ericsson.com>
2020-05-16 20:52:01 -0400

    c
    Change-Id: Ib0b257c355d1c5c2fd267fc89daba564b6139087

--- demo/makeDockerImage.ps1
+++ demo/makeDockerImage.ps1
@@ -1,9 +1,9 @@
 # Need to do this before running this script: Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
- rmdir -Force -R target/dependency
+# rmdir -Force -R target/dependency
  mkdir -Force target/dependency
  cp Dockerfile target/dependency/.
  cd target/dependency
 C:\'Program Files'\Java\jdk1.8.0_151\bin\jar -xf ../*.jar
 docker build -t lmcfrha/spring-boot-demo .
-docker push lmcfrha/spring-boot-demo
+
  
