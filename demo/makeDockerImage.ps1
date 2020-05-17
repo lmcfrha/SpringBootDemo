f27d04e95ebecf3d22efc6749b6aba4c3453db16
Frederic Hanslik <frederic.hanslik@ericsson.com>
2020-05-17 00:22:07 -0400

    c
    Change-Id: I178f715de76a2f292a5d8b1f4d9429f01b56ff9a

--- demo/makeDockerImage.ps1
+++ demo/makeDockerImage.ps1
@@ -1,5 +1,5 @@
 # Need to do this before running this script: Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
-# rmdir -Force -R target/dependency
+rmdir -Force -R target/dependency
 mkdir target/dependency
 # cp Dockerfile target/dependency/.
 cd target/dependency
